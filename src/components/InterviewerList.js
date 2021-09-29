a1efdfbe47a730d8769b22ee63eadc25a6cec6b1
denizedaza <dazadenize@gmail.com>
2021-09-29 13:20:17 -0400

    create interviewer list and list item files

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+  const { interviewers, value, onChange } = props;
+
+  const interviewersList = interviewers.map(interviewersObj => {
+    
+    return (
+      <InterviewerListItem
+        key={interviewersObj.id}
+        name={interviewersObj.name}
+        avatar={interviewersObj.avatar}
+        selected={interviewersObj.id === value}
+        setInterviewer={(event) => {onChange(interviewersObj.id)}}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewersList}</ul>
+    </section>
+  );
+}
+
+/*
+
+Our InterviewerList takes in three props:
+
+interviewers: array - an array of objects containing the information of each interviewer
+
+interviewer: number - the id of an interviewer
+
+setInterviewer: function - a function that accepts an interviewer id
+
+*/
