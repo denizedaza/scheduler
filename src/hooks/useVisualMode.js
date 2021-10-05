f39655501ac433a65410178652bd1811c360f3d2
denizedaza <dazadenize@gmail.com>
2021-10-04 23:30:20 -0400

    refactor to remove repetitive code

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
 
 function useVisualMode(initialMode) {
-  const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
 
   const transition = (newMode, replace = false) => {
      //if replace is true, push history to replace current mode; otherwise, move to last mode 
-     const newHistory = [...history];
-     replace ? newHistory[newHistory.length - 1] = newMode : newHistory.push(newMode);
-    setHistory(newHistory);
-    setMode(newMode)
+    //  const newHistory = [...history];
+    //  replace ? newHistory[newHistory.length - 1] = newMode : newHistory.push(newMode);
+    // setHistory(newHistory);
+
+    setHistory((prev) => {
+      const newHistory = [...prev];
+      replace ? newHistory[newHistory.length - 1] = newMode : newHistory.push(newMode);
+      return newHistory;
+    })
+
   }
 
   const back = () => {
     if (history.length <= 1) {
       return;
     }
-    const newHistory = [...history];
-    newHistory.pop();
-    setMode(newHistory[newHistory.length - 1]);
-    setHistory(newHistory);
+    setHistory(prev => prev.slice(0, -1));
   };
 
-   return { mode, transition, back }
+   return { mode: history[history.length - 1], transition, back }
 }
 
 export default useVisualMode;
