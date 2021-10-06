9f17376481c36cf7dd60a1837385d84907a16d8c
denizedaza <dazadenize@gmail.com>
2021-10-06 18:53:22 -0400

    add cypress for e2e testing

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,12 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday and click on it", () => {
+    cy.visit('/');
+    cy.contains('[data-testid=day]', 'Tuesday')
+      .click()
+      .should('have.class', 'day-list__item--selected');
+  })
+});
